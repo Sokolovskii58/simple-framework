b35a4ee2a3f53f670eaad046e949bc29ddea0508
Sokolovskii58 <arenda244@ya.ru>
2021-05-06 02:10:34 +0400

    Router

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 ruby '2.7.2'
 gem 'rack'
 gem 'puma'
+gem 'hanami-router'
--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
+require 'yaml'
+ROUTES = YAML.load(File.read(File.join(File.dirname(__FILE__), "app", "routes.yml"))) # 1
+
+require "./lib/router" # 2
+
 class App
+  attr_reader :router
+
+  def initialize 
+    @router = Router.new(ROUTES) # 3
+  end
+
   def call(env)
-    [200, {}, ['Hello world']]
+    result = router.resolve(env) # 4
+    [result.status, result.headers, result.content] # 5
   end
 end
