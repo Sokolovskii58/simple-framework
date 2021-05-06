7c1a9321f64a38fda8e0764faf606f01a044df7d
Sokolovskii58 <arenda244@ya.ru>
2021-05-07 02:48:54 +0400

    Migrations

--- app.rb
+++ app.rb
@@ -1,30 +1,19 @@
 require 'yaml'
-ROUTES = YAML.load(File.read(File.join(File.dirname(__FILE__), "app", "routes.yml"))) # 1
-Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|file| require file }
-Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each {|file| require file }
-
-require "./lib/router" # 2
-
-db_config_file = File.join(File.dirname(__FILE__), "app", "database.yml")
-if File.exist?(db_config_file)
-  config = YAML.load(File.read(db_config_file))
-  DB = Sequel.connect(config)
-end
+require "./lib/boot"
 
 class App
   attr_reader :router
 
   def initialize 
-    @router = Router.new(ROUTES) # 3
+    @router = Router.new(ROUTES)
   end
 
   def call(env)
-    result = router.resolve(env) # 4
-    [result.status, result.headers, result.content] # 5
+    result = router.resolve(env)
+    [result.status, result.headers, result.content]
   end
-  
+
   def self.root
     File.dirname(__FILE__)
   end
-
 end
